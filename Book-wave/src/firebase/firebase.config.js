5ca6eb2d46a67245cde4d96e68f6d358c73a4166
Sammy Obonyo <104747551+SammyObeezy@users.noreply.github.com>
2024-07-10 08:31:08 +0300

    Update firebase.config.js

--- Book-wave/src/firebase/firebase.config.js
+++ Book-wave/src/firebase/firebase.config.js
@@ -6,7 +6,7 @@ import { initializeApp } from "firebase/app";
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyDm5SCkHnoZXsFlyjXXhyDZw5HklmJi8go",
-  authDomain: 'project-portfolio-firebase.vercel.app',
+  authDomain: "book-wave-744f5.firebaseapp.com",
   projectId: "book-wave-744f5",
   storageBucket: "book-wave-744f5.appspot.com",
   messagingSenderId: "824793175947",
