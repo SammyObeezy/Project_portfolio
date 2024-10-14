fb141caed718a759ba76be8b3014ee783340ad13
mogulsam <theejaguarscomedy@gmail.com>
2024-10-14 15:07:39 +0300

    updated the book description fetching

--- Book-wave/src/shop/Shop.jsx
+++ Book-wave/src/shop/Shop.jsx
@@ -14,40 +14,14 @@ const Shop = () => {
   // Function to truncate text
   const truncateText = (text, maxLength) => {
     if (text.length <= maxLength) return text;
-    return text.slice(0, maxLength) + "...";
+    return text.slice(0, maxLength) + "..."; 
   };
 
   return (
     <div className="mt-28 px-4 lg:px-24">
-      <h2 className="text-5xl font-bold text-center">All Books are Here</h2>
+      <h2 className="text-5xl font-bold text-center">Find Your Next Read Here</h2>
       <div className="grid gap-8 my-12 lg:grid-cols-4 sm:grid-cols-2 md:grid-cols-3 grid-cols-1">
         {books.map((book) => (
-<<<<<<< HEAD
-          <>
-            {" "}
-            <Card className="max-w-sm p-4 shadow-lg">
-              <img
-                src={book.imageURL}
-                alt={book.bookTitle}
-                className="h-48 w-full object-contain rounded-md"
-              />
-              <h5 className="mt-4 mb-2 text-lg font-semibold tracking-tight text-gray-900 dark:text-white">
-                {book.bookTitle}
-              </h5>
-              <p className="font-normal text-sm text-gray-700 dark:text-gray-400 mb-4">
-                Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.
-              </p>
-              <Link
-                to={`/book/${book._id}`}
-                className="bg-orange-500 font-semibold text-white py-2 px-4 rounded hover:bg-orange-600"
-              >
-                Get A Copy
-              </Link>
-            </Card>
-
-
-          </>
-=======
           <Card key={book._id}>
             <img src={book.imageURL} alt={book.bookTitle} className="h-96" />
             <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
@@ -59,12 +33,11 @@ const Shop = () => {
             </p>
             <Link
               to={`/book/${book._id}`}
-              className="bg-orange-500 font-semibold text-white py-2 px-6 rounded text-left block w-max transition duration-300 hover:bg-orange-600"
+              className="bg-red-500 font-semibold text-white py-2 px-6 rounded text-left block w-max transition duration-300 hover:bg-orange-600"
             >
               Get A Copy
             </Link>
           </Card>
->>>>>>> a3d7af65130fde547eae0ec3aadb281dfb31eae8
         ))}
       </div>
     </div>
