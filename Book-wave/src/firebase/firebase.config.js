35475e07d166eba4a437fc59603714455d63ee38
SammyObeezy <barasam07@gmail.com>
2025-01-03 11:41:41 +0300

    Updated env files

--- Book-wave/src/firebase/firebase.config.js
+++ Book-wave/src/firebase/firebase.config.js
@@ -1,16 +1,14 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
+// Your web app's Firebase configuration using environment variables
 const firebaseConfig = {
-  apiKey: "AIzaSyDm5SCkHnoZXsFlyjXXhyDZw5HklmJi8go",
-  authDomain: "book-wave-744f5.firebaseapp.com",
-  projectId: "book-wave-744f5",
-  storageBucket: "book-wave-744f5.appspot.com",
-  messagingSenderId: "824793175947",
-  appId: "1:824793175947:web:2cec095f762eb30f10818e",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
