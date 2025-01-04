1bb41def02fb9bdbb32941d3b0c2b4b13ce6a7f0
SammyObeezy <barasam07@gmail.com>
2025-01-04 14:23:41 +0300

    Debugging

--- myapp/index.js
+++ myapp/index.js
@@ -2,7 +2,7 @@ require("dotenv").config(); // Load environment variables from .env
 
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 5000; 
+const port = process.env.PORT; 
 const cors = require("cors");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
